14e33198f68c78bc96df11362dc9394675c392cd
Tejas Joshi <tjoshi@wordandbrowncompanies.com>
2022-05-16 16:57:09 -0700

    up to event emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
-
-
+  onCourseSelected(course: Course) {
+    console.log("App component - course selected ...", course);
+  }
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Course } from "../model/course";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onCourseViewed() {
+    console.log("card component - button clicked ...");
+    this.courseSelected.emit(this.course);
+  }
+}
